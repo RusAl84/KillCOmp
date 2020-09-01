0ee172a1dbf7a6396c937f7c9ee3b1aec10fb889
Вячеслав Филатов <Filatov_VV@live.ru>
2020-09-01 19:37:38 +0300

    streamreader

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace ConsoleApp1
 {
@@ -6,17 +7,35 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Kill your computer");
-            int x = 20;
-            //
-            
+            int x = 20000;
+            string text = "";
 
-                for (int i = 1; i <= x; i++)
+
+            for (int i = 1; i <= x; i++)
+            {
+                text += "Привет мир!\nПока мир...";
+
+            }
+            string writePath = @"C:\SomeDir\hta.txt";
+            
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
                 {
-                    Console.WriteLine("Beep number {0}.", i);
-                    Console.Beep();
+                    sw.WriteLine(text);
                 }
 
+                using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine("Дозапись");
+                    sw.Write(4.5);
+                }
+                Console.WriteLine("Запись выполнена");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
         }
     }
