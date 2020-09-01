25a0e5a72df4febddd68712fffec23b335beb41e
Вячеслав Филатов <Filatov_VV@live.ru>
2020-09-01 20:46:19 +0300

    синх

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,23 +1,27 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            int x = 20000;
+            int x = 100*1000;
             string text = "";
 
+            
 
             for (int i = 1; i <= x; i++)
             {
                 text += "Привет мир!\nПока мир...";
 
             }
-            string writePath = @"C:\SomeDir\hta.txt";
-            
+
+            Stopwatch stopWatch1 = Stopwatch.StartNew();
+            string writePath = @"D:\1\hta.txt";
             try
             {
                 using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
@@ -30,13 +34,39 @@ namespace ConsoleApp1
                     sw.WriteLine("Дозапись");
                     sw.Write(4.5);
                 }
-                Console.WriteLine("Запись выполнена");
+                Console.WriteLine("Запись выполнена синхронно");
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
+            stopWatch1.Stop();
+            TimeSpan ts1 = stopWatch1.Elapsed;
+            Console.WriteLine(ts1);
+            Stopwatch stopWatch2 = Stopwatch.StartNew();
+            string writePath2 = @"D:\1\hta2.txt";
+            try
+            {
+                using (StreamWriter sw2 = new StreamWriter(writePath2, false, System.Text.Encoding.Default))
+                {
+                    await sw2.WriteLineAsync(text);
+                }
 
+                using (StreamWriter sw2 = new StreamWriter(writePath2, true, System.Text.Encoding.Default))
+                {
+                    await sw2.WriteLineAsync("Дозапись");
+                    await sw2.WriteAsync("4,5");
+                }
+                Console.WriteLine("Запись выполнена асинхронно");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            stopWatch2.Stop();
+            TimeSpan ts2 = stopWatch2.Elapsed;
+            Console.WriteLine(ts2);
+            Console.ReadLine();
         }
     }
 }
